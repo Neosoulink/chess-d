429e3eca913892797a46b721bfd5e01bd666de7e
Nathan Mande <neodarksoulink@gmail.com>
2024-12-01 17:20:37 +0200

    chore(deps): use `@vitejs/plugin-react` for compilation

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	css: {
