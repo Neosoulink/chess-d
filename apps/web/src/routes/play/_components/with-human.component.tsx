6a54bdfa1822c21bdacd89afe78ebac62ea2691e
Nathan Mande <neodarksoulink@gmail.com>
2025-03-02 18:41:54 +0300

    refactor(web): minor

--- apps/web/src/routes/play/_components/with-human.component.tsx
+++ apps/web/src/routes/play/_components/with-human.component.tsx
@@ -43,7 +43,7 @@ export const WithHumanComponent: FC<WithHumanComponentProps> = () => {
 		PlayerModel | undefined
 	>();
 
-	console.log(import.meta.env.VITE_SERVER_HOST);
+	console.log(import.meta.env);
 
 	const socket = useMemo(
 		() =>
