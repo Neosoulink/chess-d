061fb1cfba84098733657709292a402ee9e6a83b
Nathan Mande <neodarksoulink@gmail.com>
2026-02-20 15:57:04 +0300

    fix(web): correct pieces material reset

--- apps/web/src/core/game/world/chessboard/pieces/pieces.module.ts
+++ apps/web/src/core/game/world/chessboard/pieces/pieces.module.ts
@@ -1,9 +1,11 @@
+import { ChessboardModule as ChessboardModuleBase } from "@chess-d/chessboard";
 import { Module } from "@quick-threejs/reactive";
 import { inject, singleton } from "tsyringe";
 import { Subscription } from "rxjs";
 
 import { PiecesService } from "./pieces.service";
 import { PiecesController } from "./pieces.controller";
+import { EngineController } from "../../../engine/engine.controller";
 
 @singleton()
 export class PiecesModule implements Module {
@@ -11,13 +13,29 @@ export class PiecesModule implements Module {
 
 	constructor(
 		@inject(PiecesService) private readonly _service: PiecesService,
-		@inject(PiecesController) private readonly _controller: PiecesController
+		@inject(PiecesController) private readonly _controller: PiecesController,
+		@inject(ChessboardModuleBase)
+		private readonly _chessboard: ChessboardModuleBase,
+		@inject(EngineController)
+		private readonly _engineController: EngineController
 	) {
 		this._subscriptions.push(
 			this._controller.resetFen$?.subscribe((fen) =>
 				this._service.resetFen(fen)
 			),
 			this._controller.reset$?.subscribe(() => this._service.reset()),
+			this._chessboard.pieces
+				.getPieceDropped$()
+				?.subscribe(this._service.resetMaterials.bind(this._service)),
+			this._chessboard.pieces
+				.getPiecePromoted$()
+				?.subscribe(this._service.resetMaterials.bind(this._service)),
+			this._engineController.undo$.subscribe(
+				this._service.resetMaterials.bind(this._service)
+			),
+			this._engineController.redo$.subscribe(
+				this._service.resetMaterials.bind(this._service)
+			),
 			this._controller.animatedPlayerMovedPiece$?.subscribe(
 				this._service.handleAnimatedPlayerMovedPiece.bind(this._service)
 			),
